2c988d2908c620bf110de31cd38dab5d262acf44
Paco <francisco.albinana@iesnervion.es>
2022-10-04 08:35:46 +0200

    Commit 2 04/10

--- Desarrollo de Interfaces (DEINT)/05-Hola_Mundo_Ej1/05-Hola_Mundo_Ej1/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _05_Hola_Mundo_Ej1;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- Desarrollo de Interfaces (DEINT)/05-Hola_Mundo_Ej1/05-Hola_Mundo_Ej1/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace _05_Hola_Mundo_Ej1;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
