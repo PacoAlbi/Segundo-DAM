977e96a278b4c117f5f6caa0ba3f82ba958a8ec7
Paco <francisco.albinana@iesnervion.es>
2022-10-06 14:33:03 +0200

    commit 06/10 6
    Ultima Hora

--- Sistemas de Gestión Empresarial (SGEMP)/06-Hola_Mundo_MVC/06-Hola_Mundo_MVC_UI/Controllers/HomeController.cs
+++ Sistemas de Gestión Empresarial (SGEMP)/06-Hola_Mundo_MVC/06-Hola_Mundo_MVC_UI/Controllers/HomeController.cs
@@ -4,7 +4,17 @@ namespace _06_Hola_Mundo_MVC_UI.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public String Index()
+        {
+            return "Hola Mundo desde el controlador";
+        }
+
+        public String Paco()
+        {
+            return "Hola Paco desde el controlador";
+        }
+
+        public ViewResult VistaHolaMundo ()
         {
             return View();
         }
