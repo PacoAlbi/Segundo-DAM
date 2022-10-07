6b9a4691d3794102d7cfc0ccdc729bab2715fc91
Paco <francisco.albinana@iesnervion.es>
2022-10-07 11:13:11 +0200

    07/10 recreo

--- Acceso a datos (ACDAT)/Ejercicio1_2c/src/Main.java
+++ Acceso a datos (ACDAT)/Ejercicio1_2c/src/Main.java
@@ -1,39 +1,77 @@
-
 import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        File file = new File("src\\a.txt");
-        File fileW = new File("src\\cincoA.txt");
+        File file = new File("src\\letras.txt");
+        File fileW = new File("src\\escritura.txt");
         try {
             RandomAccessFile rafW = new RandomAccessFile(fileW, "rw");
             RandomAccessFile rafR = new RandomAccessFile(file, "r");
 
-            /*
-            Mejor que lo lea en entero porque
-            si recoge los bytes y se mueve a la siguiente posición se moverá a donde necesitamos
-            En cambio, si lo hacemos con Char machacará la segunda posición del byte[] que lo compone
-             */
-
-
-            int a = 0;
-            int j = 12;
-            while (j >= 0) {
-                rafR.seek(j);
+            int a;
+            int j = 0;
+            while (j <= 4) {
+                rafR.seek(0);
                 a = rafR.read();
-                if (j % 2 != 0) {
-                    j--;
-                } else {
-                    rafW.write(a);
-                    j--;
-                }
-
-
+                rafW.write(a);
+                j++;
             }
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
+    public static void burbuja(int[] A) {
+        int i, j, aux;
+        for (i = 0; i < A.length - 1; i++) {
+            for (j = 0; j < A.length - i - 1; j++) {
+                if (A[j + 1] < A[j]) {
+                    aux = A[j + 1];
+                    A[j + 1] = A[j];
+                    A[j] = aux;
+                }
+            }
+        }
+    }
+
+
+    public static void Eva() {
+
+
+        File file = new File("src\\abecedario.txt");
+        File file2 = new File("abecedarioAlReves.txt");
+
+
+        try {
+
+            RandomAccessFile randomAccessFileLectura = new RandomAccessFile(file, "r");
+            RandomAccessFile randomAccessFileEscritura = new RandomAccessFile(file2, "rw");
+            int letra, texto;
+            int i = 0;
+
+
+            while ((letra = randomAccessFileLectura.read()) != -1) {
+
+
+                if (i % 2 != 0) {
+                    randomAccessFileEscritura.writeBytes(System.lineSeparator());
+                    i++;
+                }
+
+
+                // conseguir que imprima al reves
+
+                randomAccessFileEscritura.write(letra);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    
 }
