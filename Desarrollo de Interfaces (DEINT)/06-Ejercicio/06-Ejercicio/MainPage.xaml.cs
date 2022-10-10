e838f323b4b273471ec371f466a9961f528651dd
Paco <francisco.albinana@iesnervion.es>
2022-10-10 12:12:29 +0200

    10/10 Despues del recreo

--- Desarrollo de Interfaces (DEINT)/06-Ejercicio/06-Ejercicio/MainPage.xaml.cs
+++ Desarrollo de Interfaces (DEINT)/06-Ejercicio/06-Ejercicio/MainPage.xaml.cs
@@ -4,27 +4,27 @@ namespace _06_Ejercicio;
 public partial class MainPage : ContentPage
 {
 
-
+    /// <summary>
+    /// Evento principal que inicializa los componenetes.
+    /// Precondiciones: No tiene
+    /// Postcondiciones: No tiene
+    /// </summary>
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
+    /// <summary>
+    /// Precondiciones: No tiene
+    /// Evento asociado al click del botón 2, que cada vez que lo pulsas te crea un nuevo botón.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// Postcondiciones: Genera un nuevo botón cada vez.
     private void btn2_Clicked(object sender, EventArgs e)
     {
-        if (btn3.IsVisible == true)
-        {
-            btn3.IsVisible = false;
-        }
-        else
-        {
-            btn3.IsVisible = true;
-        }
-
-
-        Button btn;
-
-        btn = new Button
+        
+        Button btn = new Button
         {
             Text = "Boton4",
             BackgroundColor = Colors.Blue,
@@ -39,12 +39,19 @@ public partial class MainPage : ContentPage
         };
 
         btn.Clicked += btn3_Clicked;
-
+        layout.Children.Add(btn);
     }
 
+    /// <summary>
+    /// Precondiciones: No tiene
+    /// Evento asociado al click del botón 3, que cuando lo pulsas, elimina el botón 1 y cambia el texto del botón 2
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    /// Postcondiciones: Genera un nuevo botón cada vez.
     private void btn3_Clicked(object sender, EventArgs e)
     {
-        btn1.IsVisible = false;
+        layout.Children.Remove(btn1);
         btn2.Text = "Crear controles mola como una gramola";
         btn2.WidthRequest = 400;
     }
