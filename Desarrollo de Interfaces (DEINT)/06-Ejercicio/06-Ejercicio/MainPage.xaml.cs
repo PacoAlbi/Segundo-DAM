8b3c48954ccb481b950cba43edd8e902697150f6
Paco <francisco.albinana@iesnervion.es>
2022-10-10 11:08:54 +0200

    10/10 antes recreo

--- Desarrollo de Interfaces (DEINT)/06-Ejercicio/06-Ejercicio/MainPage.xaml.cs
+++ Desarrollo de Interfaces (DEINT)/06-Ejercicio/06-Ejercicio/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace _06_Ejercicio;
+﻿
+namespace _06_Ejercicio;
 
 public partial class MainPage : ContentPage
 {
@@ -9,6 +10,43 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+    private void btn2_Clicked(object sender, EventArgs e)
+    {
+        if (btn3.IsVisible == true)
+        {
+            btn3.IsVisible = false;
+        }
+        else
+        {
+            btn3.IsVisible = true;
+        }
 
+
+        Button btn;
+
+        btn = new Button
+        {
+            Text = "Boton4",
+            BackgroundColor = Colors.Blue,
+            WidthRequest = 200,
+            HeightRequest = 70,
+            HorizontalOptions = LayoutOptions.Center,
+            VerticalOptions = LayoutOptions.Center,
+            FontSize = 16,
+            FontAttributes = FontAttributes.Bold,
+            BorderColor = Colors.Yellow,
+            Margin = 30,
+        };
+
+        btn.Clicked += btn3_Clicked;
+
+    }
+
+    private void btn3_Clicked(object sender, EventArgs e)
+    {
+        btn1.IsVisible = false;
+        btn2.Text = "Crear controles mola como una gramola";
+        btn2.WidthRequest = 400;
+    }
 }
 
