69ec66031aeb19498d9833e1cecd702a913f55e7
Paco <francisco.albinana@iesnervion.es>
2022-10-13 11:12:08 +0200

    13/10 Antes recreo

--- Programación de Servicios y Procesos (PSPRO)/ProcesosTema1/src/Ejercicio1_5/PreguntaNombre.java
+++ Programación de Servicios y Procesos (PSPRO)/ProcesosTema1/src/Ejercicio1_5/PreguntaNombre.java
@@ -6,6 +6,7 @@ public class PreguntaNombre {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe tu nombre");
         String nombre = sc.nextLine();
         System.out.printf("Hola %s", nombre);
         sc.close();
--- Programación de Servicios y Procesos (PSPRO)/ProcesosTema1/src/Ejercicio1_9/Main.java
+++ Programación de Servicios y Procesos (PSPRO)/ProcesosTema1/src/Ejercicio1_9/Main.java
@@ -1,12 +1,12 @@
-package Ejercicio1_8;
+package Ejercicio1_9;
 
 import java.io.*;
 
-public class Prueba {
+public class Main {
 
     public static void main(String[] args) {
         Runtime r = Runtime.getRuntime();
-        String [] comando = {"java", "src/ProcesoLento/ProcesoLento.java"};
+        String [] comando = {"java", "src/Ejercicio1_5/PreguntaNombre.java"};
 
         try {
             Process p = r.exec(comando);
@@ -15,12 +15,11 @@ public class Prueba {
             BufferedReader br = new BufferedReader(isr);
 
 
-            BufferedWriter bw = new BufferedWriter(new FileWriter("src/Ejercicio1_8/salida.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("src/Ejercicio1_8/salidaProcesoLento2.txt"));
 
             String line = br.readLine();
             while (line!=null){
-                //System.out.println(line); para probar que sale por pantalla,
-                bw.write(line+System.lineSeparator());
+                System.out.println(line);
                 line = br.readLine();
             }
 
