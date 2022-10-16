c15bdd5496bb1da199397ffe5c7d078611a8a78e
Paco <francisco.albinana@iesnervion.es>
2022-10-16 21:03:55 +0200

    16/10 21:00

---
+++ Desarrollo de Interfaces (DEINT)/07-Pages/07-Navigation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _07_Navigation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
