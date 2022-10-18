4fb93c5b597855e6977af52dd994976dca52d755
Paco <francisco.albinana@iesnervion.es>
2022-10-19 00:06:28 +0200

    23:59

--- Desarrollo de Interfaces (DEINT)/07-Pages/07-Pages/MainPage.xaml.cs
+++ Desarrollo de Interfaces (DEINT)/07-Pages/07-Pages/MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using _07_Pages.Modelos;
+using _07_Pages.Views;
+using Microsoft.Maui.Controls;
 
 namespace _07_Pages;
 
@@ -9,23 +11,24 @@ public partial class MainPage : ContentPage
     public MainPage()
 	{
 		InitializeComponent();
+
         entryNombre.BindingContext = persona.Name;
         entryApellidos.BindingContext = persona.Apellidos;
 	}
 
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-        await Navigation.PushAsync(new Views.PaginaTabbed());
+        await Navigation.PushAsync(new PaginaTabbed());
 	}
 
     private async void OnCounterClicked4(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Views.Pagina4());
+        await Navigation.PushAsync(new Pagina4());
         
     }
 
     private async void OnCounterClicked5(object sender, EventArgs e)
     {
-        await Navigation.PushAsync(new Views.Pagina5());
+        await Navigation.PushAsync(new Pagina5{ BindingContext = persona });
     }
 }
--- Desarrollo de Interfaces (DEINT)/07-Pages/07-Pages/Views/Pagina4.xaml.cs
+++ Desarrollo de Interfaces (DEINT)/07-Pages/07-Pages/Views/Pagina4.xaml.cs
@@ -7,5 +7,8 @@ public partial class Pagina4 : ContentPage
 	public Pagina4()
 	{
 		InitializeComponent();
+
+		//lblPagina4.Text = $"Hola {persona.Name} {persona.Apellidos}";
+		//lblPagina4.SetBinding(Label.ScaleProperty, new Binding("Text", source: nombre+" "+apellido));
 	}
 }
