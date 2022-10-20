40ca2871fc39a0743d697cc916095db9a3c7e12b
Paco <francisco.albinana@iesnervion.es>
2022-10-20 09:51:35 +0200

    20/10/2022 9:51

--- Programacion_Multimedia_Dispositivos_Moviles_(PMDMO)/fragmentoColores/app/src/main/java/com/example/fragmentocolores/MainActivity.kt
+++ Programacion_Multimedia_Dispositivos_Moviles_(PMDMO)/fragmentoColores/app/src/main/java/com/example/fragmentocolores/MainActivity.kt
@@ -14,36 +14,40 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity(), Comunicador {
 
-    private var contador = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    override fun onClickRojoSuma() {
+    /*override fun onClickRojoSuma() {
         var suma = findViewById<TextView>(R.id.textView)
         contador++
         suma.text = contador.toString()
-    }
+    }*/
 
-    override fun onClickVerdeResta() {
-        Toast.makeText(this, "Has pulsado", Toast.LENGTH_SHORT) //El toast no funciona por el contexto
-        val valor = this.findViewById<TextView>(R.id.textView)
-        val resto = Integer.parseInt(valor.text.toString())-1
-        if (contador == 10){
+    override fun onClickRojoSuma() {
+        val valor = this.findViewById<TextView>(R.id.textView) //capturo el valor del texbox
+        val suma = Integer.parseInt(valor.text.toString())+1    //lo actuliazo directamente
+        if (suma == 10){
             AlertDialog.Builder(this)
                 .setTitle("¿Para Pesao!")
                 .setMessage("Ya van 10")
                 .setPositiveButton("Si, quiero rayarte"){dialog, which->
-                    valor.setText(contador.toString())
+                    valor.setText(suma.toString())
                 }
                 //Ponemos null y se cierra solo
                 .setNegativeButton("Paso", null)
                 .setIcon(android.R.drawable.btn_dialog)
                 .show()
-        }else{
-            valor.setText(resto.toString())
+        }else {
+            valor.setText(suma.toString()) //devuelvo el valor cambiado
         }
     }
+
+    override fun onClickVerdeResta() {
+        Toast.makeText(this, "Has pulsado", Toast.LENGTH_SHORT) //El toast no funciona por el contexto
+        val valor = this.findViewById<TextView>(R.id.textView)
+        val resto = Integer.parseInt(valor.text.toString())-1 //Pillo el valor de la casilla mejor, asi se actualiza mejor
+        valor.setText(resto.toString())
+        }
 }
