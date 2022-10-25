483470ba740b4870722f5571bdbde0ae96a1c456
Paco <francisco.albinana@iesnervion.es>
2022-10-25 11:08:23 +0200

    11:08

--- Sistemas de Gestión Empresarial (SGEMP)/07_ASP_Net_MVC/07_ASP_Net_MVC/Models/ViewModels/clsEditarPersonaViewModel.cs
+++ Sistemas de Gestión Empresarial (SGEMP)/07_ASP_Net_MVC/07_ASP_Net_MVC/Models/ViewModels/clsEditarPersonaViewModel.cs
@@ -5,8 +5,13 @@ namespace _07_ASP_Net_MVC.Models.ViewModels
 {
     public class clsEditarPersonaViewModel
     {
-        public static List<clsDepartamento> listaDepartamentos = clsListadoDepartamentos.listarDepartamentos();
-        public static clsPersona persona;
+        public static List<clsDepartamento> listaDepartamentos { get; set; }
+        public static clsPersona persona { get; set; }
 
+        public clsEditarPersonaViewModel(clsPersona editarpersona, List<clsDepartamento> lista)
+        {
+            persona = editarpersona;
+            listaDepartamentos = lista;
+        }
     }
 }
