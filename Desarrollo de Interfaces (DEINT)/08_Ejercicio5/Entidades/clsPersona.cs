30f1b0a40e2875ad67722e1729e9f809a2306b78
Paco <francisco.albinana@iesnervion.es>
2022-10-26 14:36:11 +0200

    14:36

---
+++ Desarrollo de Interfaces (DEINT)/08_Ejercicio5/Entidades/clsPersona.cs
@@ -0,0 +1,39 @@
+﻿namespace Entidades
+{
+    // All the code in this file is included in all platforms.
+    public class clsPersona
+    {
+        #region Atributos
+        private string nombre;
+        private string apellido;
+        #endregion
+
+        #region Constructores
+        public clsPersona() { }
+        public clsPersona(string nombre, string apellido)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+        #endregion
+
+        #region Getter y Setter
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre=nombre;
+        }
+        public string GetApellido()
+        {
+            return apellido;
+        }
+        public void SetApellido(string apellido)
+        {
+            this.apellido = apellido;
+        }
+        #endregion
+    }
+}
