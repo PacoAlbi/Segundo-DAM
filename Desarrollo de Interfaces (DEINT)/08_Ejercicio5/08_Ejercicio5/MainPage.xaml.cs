00d955fbbcfa41f51d74f759fa7950b7b18be7a8
Paco <francisco.albinana@iesnervion.es>
2022-10-27 18:19:12 +0200

    27/10 18:19

--- Desarrollo de Interfaces (DEINT)/08_Ejercicio5/08_Ejercicio5/MainPage.xaml.cs
+++ Desarrollo de Interfaces (DEINT)/08_Ejercicio5/08_Ejercicio5/MainPage.xaml.cs
@@ -1,11 +1,17 @@
-﻿namespace _08_Ejercicio5
+﻿using _08_Ejercicio5.Models.DAL;
+using Entidades;
+using System.Collections.ObjectModel;
+
+namespace _08_Ejercicio5
 {
 	public partial class MainPage : ContentPage
-	{     
+	{
+
+        //ObservableCollection personas = new ObservableCollection();
+        
         public MainPage()
 		{
 			InitializeComponent();
-            
         }
     }
 }
---
+++ Desarrollo de Interfaces (DEINT)/08_Ejercicio5/08_Ejercicio5/Models/DAL/ObservableCollection.cs
@@ -0,0 +1,18 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Ejercicio5.Models.DAL
+{
+    internal class ObservableCollection
+    {
+        public ObservableCollection() { }
+
+        //public ObservableCollection(IEnumerable<string> items) { }
+
+        //public ObservableCollection Personas { get { return new(clsPersona); } }
+    }
+}
