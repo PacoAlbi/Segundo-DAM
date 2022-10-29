2dc9ee3352df4fc3916e587b64dc15170398f728
Paco <francisco.albinana@iesnervion.es>
2022-10-29 13:44:23 +0200

    13:44

--- Desarrollo de Interfaces (DEINT)/08_Ejercicio5/DAL/ListadoCompletoPersonas.cs
+++ Desarrollo de Interfaces (DEINT)/08_Ejercicio5/DAL/ListadoCompletoPersonas.cs
@@ -8,25 +8,23 @@ namespace DAL
     {
 
         /// <summary>
-        /// Funcion que devuelve una lista de personas
+        /// Método que crea una lista de personas y se la pasa al controlador para ser mostrada.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>Devuelve un ObservableCollection de personas.</returns>
         public static ObservableCollection<clsPersona> obtenerListadoPersonas()
         {
             ObservableCollection<clsPersona> listaPersonas = new ObservableCollection<clsPersona>();
 
-
-
-            listaPersonas.Add(new clsPersona("Eva", "Ramos"));
-            listaPersonas.Add(new clsPersona("Virginia", "Delgado"));
-            listaPersonas.Add(new clsPersona("Manuel", "Lopez"));
             listaPersonas.Add(new clsPersona("Paco", "Albiñana"));
+            listaPersonas.Add(new clsPersona("Eva", "Ramos"));
+            listaPersonas.Add(new clsPersona("Pepe", "Perez"));
+            listaPersonas.Add(new clsPersona("Manuel", "Lopez"));            
             listaPersonas.Add(new clsPersona("Carmelo", "Aguilar"));
-            listaPersonas.Add(new clsPersona("Pedro", "Cornejo"));
-            listaPersonas.Add(new clsPersona("Xisca", "Rumier"));
-            listaPersonas.Add(new clsPersona("Rosario", "Leon"));
-            listaPersonas.Add(new clsPersona("Jesus Miguel", "Ramos"));
-            listaPersonas.Add(new clsPersona("Carlos", "Rupias"));
+            listaPersonas.Add(new clsPersona("Fernando", "Galiana"));
+            listaPersonas.Add(new clsPersona("Pedro", "Cornejo"));           
+            listaPersonas.Add(new clsPersona("Marta", "Devos"));
+            listaPersonas.Add(new clsPersona("Rocio", "Campos"));
+            listaPersonas.Add(new clsPersona("Laura", "Alcazar"));
 
             return listaPersonas;
         }
--- Desarrollo de Interfaces (DEINT)/08_Ejercicio5/Entidades/clsPersona.cs
+++ Desarrollo de Interfaces (DEINT)/08_Ejercicio5/Entidades/clsPersona.cs
@@ -1,38 +1,21 @@
 ﻿namespace Entidades
 {
-    // All the code in this file is included in all platforms.
+    /// <summary>
+    /// Clase base o entidad de persistencia
+    /// </summary>
     public class clsPersona
     {
         #region Atributos
-        private string nombre;
-        private string apellido;
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
         #endregion
 
         #region Constructores
         public clsPersona() { }
         public clsPersona(string nombre, string apellido)
         {
-            this.nombre = nombre;
-            this.apellido = apellido;
-        }
-        #endregion
-
-        #region Getter y Setter
-        public string GetNombre()
-        {
-            return nombre;
-        }
-        public void SetNombre(string nombre)
-        {
-            this.nombre=nombre;
-        }
-        public string GetApellido()
-        {
-            return apellido;
-        }
-        public void SetApellido(string apellido)
-        {
-            this.apellido = apellido;
+            Nombre = nombre;
+            Apellido = apellido;
         }
         #endregion
     }
