3fc3e572f4ab6669d41f1f327c8533b578d2c757
Paco <francisco.albinana@iesnervion.es>
2022-11-16 11:05:45 +0100

    16/11 11:05

---
+++ SGEMP/10_Ejercicio1/10_Ejercicio1_ASP/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using _10_Ejercicio1_ASP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+
+
+namespace _10_Ejercicio1_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName ("Index")]
+        public IActionResult IndexPost()
+        {
+            
+            SqlConnection miConexion = new SqlConnection();
+            try
+
+            {
+
+                miConexion.ConnectionString = "server=falbinana.database.windows.net;database=PacoBBDD;uid=falbinana;pwd=Paquete6";
+                miConexion.Open();
+                ViewBag.estadoConexion = miConexion.State;
+
+            }
+            catch (Exception e)
+            { //TO DO
+            }
+            finally { miConexion?.Close(); }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
