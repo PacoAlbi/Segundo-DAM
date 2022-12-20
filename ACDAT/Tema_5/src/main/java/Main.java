f28d661f417011da584b77bdd34e61a5f080edea
Paco <francisco.albinana@iesnervion.es>
2022-12-20 10:03:36 +0100

    20/12 10:00

--- ACDAT/Tema_5/src/main/java/Main.java
+++ ACDAT/Tema_5/src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             setUp();
-            guardar("Evita Peron", 2000.00);
+            guardar("Pedro duque", 7412.00);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
