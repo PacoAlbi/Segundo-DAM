2a67648e289937539c40f24316154955320a487e
Paco <francisco.albinana@iesnervion.es>
2023-01-16 22:14:30 +0100

    22:14

---
+++ SGEMP/EjerciciosJavaScript/EjerciciosJavaScript/wwwroot/js/Ejercicio1.js
@@ -0,0 +1,23 @@
+﻿window.onload = inicializaEventos;
+
+class Persona {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+function inicializaEventos() {
+
+    document.getElementById("btnSaludo").onclick = saludar;
+
+    //document.getElementById("btnSaludo").addEventListener("click", saludar, false); Este hace lo mismo que el de arriba
+
+}
+
+function saludar() {
+
+    var persona = new Persona(document.getElementById("txtNombre").value, document.getElementById("txtApellido").value)
+    alert("Hola " + persona.nombre + " " + persona.apellido);
+
+}
---
+++ SGEMP/EjerciciosJavaScript/EjerciciosJavaScript/wwwroot/js/Ejercicio3.js
@@ -0,0 +1,47 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos() {
+
+    cargarComidas();
+    cargarPlatos();
+    //document.getElementById("btnComidas").onselect = cargarPlatos(document.getElementById("btnComidas").option);
+
+}
+
+function cargarComidas() {
+
+    var arrayComidas = ["Pizzas", "Hamburguesas", "Pastas"];
+    //arrayComidas.sort(); Puedo ordenar si quiero.
+    anadirOpciones("comidas", arrayComidas);
+}
+
+function anadirOpciones(domelement, array) {
+    var select = document.getElementsByName(domelement)[0];
+    for (value in array) {
+        var option = document.createElement("option");
+        option.text = array[value];
+        select.add(option);
+    }
+}
+
+function cargarPlatos() {
+
+    let selectComidas = document.getElementById("btnComidas");
+    let platos = selectComidas.options[selectComidas.selectedIndex].text;
+
+    var arrayPizzas = ["Proscciuto", "Cuatro estaciones", "BBQ"];
+    var arrayHamburguesas = ["BLT", "Whopper", "BaconQueso"];
+    var arrayPastas = ["Macarrones", "Spaguetti", "Tallarines"];
+
+    switch (platos) {
+        case "Pizzas":
+            anadirOpciones("platos", arrayPizzas);
+            break;
+        case "Hamburguesas":
+            anadirOpciones("platos", arrayHamburguesas);
+            break;
+        case "Pastas":
+            anadirOpciones("platos", arrayPastas);
+            break;
+    } 
+}
