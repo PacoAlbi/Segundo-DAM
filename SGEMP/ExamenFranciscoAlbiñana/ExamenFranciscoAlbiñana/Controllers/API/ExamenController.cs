708646f02a30f2a408896f399897d4bc54cd413f
Paco <francisco.albinana@iesnervion.es>
2023-02-06 12:06:36 +0100

    06/02 12:06

---
+++ SGEMP/ExamenFranciscoAlbiñana/ExamenFranciscoAlbiñana/Controllers/API/ExamenController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExamenFranciscoAlbiñana.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamenController : ControllerBase
+    {
+        // GET: api/<ExamenController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ExamenController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ExamenController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ExamenController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ExamenController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
