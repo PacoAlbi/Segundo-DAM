17e84a5a7c094ddf8f9ba264a98163dd1266a04d
Paco <francisco.albinana@iesnervion.es>
2023-02-09 13:16:31 +0100

    angular3

---
+++ SGEMP/Angular/Tema14_Pruebas/HolaMundo/src/app/interfaces/persona.ts
@@ -0,0 +1,10 @@
+export interface Persona {
+    id: number;
+    nombre: String;
+    apellidos: String;
+    telefono: String;
+    direccion: String;
+    foto: String;
+    fechaNacimiento: Date;
+    idDepartamento: number;
+}
