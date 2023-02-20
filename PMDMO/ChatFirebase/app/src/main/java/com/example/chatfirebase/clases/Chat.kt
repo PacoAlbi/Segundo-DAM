1a1422ebcf2757e38dceff06f3d8ee243c8004c3
Paco <francisco.albinana@iesnervion.es>
2023-02-20 08:57:21 +0100

    20/02 9

--- PMDMO/ChatFirebase/app/src/main/java/com/example/chatfirebase/clases/Chat.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chatfirebase.clases
-
-data class Chat(
-    var id: String = "",
-    var nombre: String = "",
-    var usuarios: List<String> = emptyList()
-)
--- PMDMO/ChatFirebase/app/src/main/java/com/example/chatfirebase/clases/Mensaje.kt
+++
@@ -1,9 +0,0 @@
-package com.example.chatfirebase.clases
-
-import java.util.*
-
-data class Mensaje (
-    var texto: String = "",
-    var de: String = "",
-    var fecha: Date = Date()
-)
