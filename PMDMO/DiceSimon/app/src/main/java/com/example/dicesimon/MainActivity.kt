808882baec31daaea0e2587731cfb366e42c1411
Paco <francisco.albinana@iesnervion.es>
2023-03-08 13:23:57 +0100

    08/03 13:25

--- PMDMO/DiceSimon/app/src/main/java/com/example/dicesimon/MainActivity.kt
+++ PMDMO/DiceSimon/app/src/main/java/com/example/dicesimon/MainActivity.kt
@@ -7,5 +7,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragmentAmarillo, AmarilloFragment())
+        fragmentTransaction.commit()
+
+
     }
+
+    fun secuenciaJuego(): MutableList<String> {
+        val colores = listOf("Amarillo","Azul","Rojo","Verde").random()
+        val secuencia = mutableListOf<String>()
+        when (colores) {
+            "Amarillo" -> secuencia.add("Amarillo")
+            "Azul" -> secuencia.add("Azul")
+            "Rojo" -> secuencia.add("Rojo")
+            "Verde" -> secuencia.add("Verde")
+        }
+        return secuencia
+    }
+
+
 }
